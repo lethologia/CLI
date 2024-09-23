9634c69d4370174cd480e91eb7391dc6bfca5c53
lethologia <88973126+lethologia@users.noreply.github.com>
2024-09-23 20:49:29 +0800

    Improved

--- cli.c
+++ cli.c
@@ -1,41 +1,113 @@
-#include "cli.h"
 #include <stdio.h>
+#include <stdarg.h>
+#include <ctype.h>
 
-char* menu_labels[] = {"Test1", "Test2"};
-menu_f menu_funcs[] = {&test1, &test2};
+
+#include "cli.h"
+
+
 
 // Used for testing purposes
 int main(int argc, char** argv) {
-    if (labels_length() != funcs_length())
-        return 1; 
-
-    printf("=============\n");
-    printf("  CANMAN V1  \n");
-    printf("=============\n");
-
-    int i;
-    for (i = 0; i < labels_length(); i++) {
-        printf("1. %s\n", menu_labels[i]);
+    while (1) {
+        root();
     }
-
-    (menu_funcs[0])();
-    (menu_funcs[1])();
 
     return 0;
 }
 
-inline size_t labels_length() {
-    return sizeof(menu_labels)/sizeof(*menu_labels);
+// Main menu
+void root() {
+    print_banner();
+
+    menu_navigation(root_items, _countof(root_items));
 }
 
-inline size_t funcs_length() {
-    return sizeof(menu_funcs)/sizeof(*menu_funcs);
+// root sub-menus
+void root_monitor() {
+    print("<Monitor>\n");
 }
 
-void test1() {
-    printf("Test 1 has been called\n");
+// Configure Menu
+void root_configure() {
+    print_banner();
+    print_heading("Configure");
+
+    menu_navigation(configure_items, _countof(configure_items));
 }
 
-void test2() {
-    printf("Test 2 has been called\n");
+// Configure sub-menus
+void configure_setname() {
+
+}
+
+void configure_setid() {
+
+}
+
+void configure_calibrate_adc() {
+
+}
+
+void configure_samplerate() {
+
+}
+
+void configure_outputrate() {
+
+}
+
+void configure_verbosity() {
+
+}
+
+
+// IO Wrapper Definitions.
+static void print(const char* string, ...) {
+    // printf wrapper
+    va_list args;
+    va_start(args, string);
+    vprintf(string, args);
+    va_end(args);
+}
+
+static void get_input(char* buffer, size_t buffer_size) {
+    // scanf wrapper
+    fgets(buffer, buffer_size, stdin);
+}
+
+static void clear() {
+    // windows clear screen
+    system("cls");
+}
+
+// Core Function Definitions.
+void print_banner() {
+    print("=======================\n");
+    print("====   CANMAN-V1   ====\n");
+    print("=======================\n\n");
+}
+
+void print_heading(const char* head) {
+    print("%s\n", head);
+}
+
+void menu_navigation(menu_item* menu, size_t length) {
+    char buffer[64];
+    
+    for (int i = 0; i < length; i++) {
+        print("%d. %s\n", i + 1, menu[i].label);
+    }
+
+    while (1) {
+        print("\n> ");
+        get_input(buffer, sizeof(buffer));
+
+        if (isdigit(buffer[0]) && atoi(buffer[0]) > 0 && atoi(buffer[0]) < length)
+            break;
+    }
+
+    clear();
+
+    (menu[atoi(buffer) - 1].func)();
 }
--- cli.h
+++ cli.h
@@ -3,12 +3,56 @@
 
 #include <stdio.h>
 
+#define _countof(array) (sizeof(array) / sizeof(array[0]))
+
+// function pointer typedef
 typedef void (*menu_f)();
 
-size_t labels_length();
-size_t funcs_length();
+// menu item datastructure
+typedef struct menu_item {
+	char* label;
+	menu_f func;
+} menu_item;
 
-void test1();
-void test2();
+// Core CLI functions.
+void print_banner();
+void print_heading(const char* head);
+void menu_navigation(menu_item* menu, size_t length);
+
+// IO wrapper functions defined depending on the platform. Should be defined properly prior to compilation.
+static void print(const char* string, ...);
+static void get_input(char* buffer, size_t buffer_size);
+static void clear();
+
+
+// ---------------------------------------------
+// Menu Definition Section.
+
+// Root Menu
+void root();
+void root_monitor();
+void root_configure();
+
+menu_item root_items[] = {
+	{"Monitor", &root_monitor},
+	{"Configure", &root_configure},
+};
+
+// Configure Menu
+void configure_setname();
+void configure_setid();
+void configure_calibrate_adc();
+void configure_samplerate();
+void configure_outputrate();
+void configure_verbosity();
+
+menu_item configure_items[] = {
+	{"Set Node Name", &configure_setname},
+	{"Set CAN ID", &configure_setid},
+	{"Calibrate ADC", &configure_calibrate_adc},
+	{"Set Sample Rate", &configure_samplerate},
+	{"Set Output Rate", &configure_outputrate},
+	{"Set Log Verbosity", &configure_verbosity},
+};
 
 #endif
